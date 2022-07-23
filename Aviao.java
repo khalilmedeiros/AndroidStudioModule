7b9257cd809c44c38dae4288fcb2a08f39737257
khalilgibran <kgnm@icomp.ufam.edu.br>
2022-07-23 10:28:25 -0400

    Adicionado Getters e Setters da classe Aviao

--- Aviao.java
+++ Aviao.java
@@ -1,7 +1,33 @@
 
 public class Aviao{
+    private String modelo;
+    private String identificador;
+    private boolean motor;
+    private float altura;
 
     public void ImprimaOk(){
         System.out.println("OK");
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public String getIdentificador(){
+        return this.identificador;
+    }
+    public boolean getMotor(){
+        return this.motor;
+    }
+    public float getAltura(){
+        return this.altura;
+    }
+    public void setModelo(String modelo){
+        this.modelo=modelo;
+    }
+    public void setIndentificador(String identificador){
+        this.identificador=identificador;
+    }
+    public void setMotor(boolean motor){
+        this.motor=motor;
     }
 }
