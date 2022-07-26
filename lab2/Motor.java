19a329a2b33c9ce6761662fa1751bed5e2892aff
khalilgibran <kgnm@icomp.ufam.edu.br>
2022-07-26 18:52:12 -0400

    Adicionando Class Motor

---
+++ lab2/Motor.java
@@ -0,0 +1,32 @@
+public class Motor{
+    private String modelo;
+    private float potencia;
+    private Boolean ativo;
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public float getPotencia(){
+        return this.potencia;
+    }
+    public Boolean getAtivo(){
+        return this.ativo;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public void setPotencia(float potencia){
+        this.potencia=potencia;
+    }
+    public void setAtivo(Boolean ativo){
+        this.ativo=ativo;
+    }
+
+    public void ligar(){
+
+    }
+    public void desligar(){
+        
+    }
+
+}
